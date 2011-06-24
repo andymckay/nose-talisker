19fc08fe7e28c4fcf6ee764e63d3067261f53ee2
Andy McKay <amckay@mozilla.com>
2011-06-24 15:34:50 -0700

    fix for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nosetalisker',
-    version='0.1',
+    version='0.2',
     description='',
     long_description='',
     author='Andy McKay',
